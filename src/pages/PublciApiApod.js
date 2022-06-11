22842cf171917821f983e834313eb7a1ce51b5c0
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-06-11 10:55:38 +0100

    updated readme file

--- src/pages/PublciApiApod.js
+++ src/pages/PublciApiApod.js
@@ -1,12 +1,16 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Button from "../components/Button";
 
 export default function PublciApiApod() {
   const { REACT_APP_KEY } = process.env;
   const apiKey = REACT_APP_KEY;
+
+  // NASA API endpoint
   const publicApi_Url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
+  // Make a get request to access/view NASA API data
   const response = axios.get(`${publicApi_Url}`, {
     withCredentials: false,
   });
@@ -18,6 +22,7 @@ export default function PublciApiApod() {
   const [title, setTitle] = useState();
   const [date, setDate] = useState();
 
+  // retrieve all relevant API data from NASA API
   async function fetchData() {
     setTitle((await response).data.title);
     setUrl((await response).data.url);
@@ -30,6 +35,7 @@ export default function PublciApiApod() {
       <h1>{title}</h1>
       <img src={url} />
       <p>Date of capture: {date}</p>
+      <Button />
     </div>
   );
 }
--- src/pages/PublicApiMars.js
+++ src/pages/PublicApiMars.js
@@ -1,12 +1,16 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Button from "../components/Button";
 
 export default function PublicApiMars() {
   const { REACT_APP_KEY } = process.env;
   const apiKey = REACT_APP_KEY;
+
+  // NASA API endpoint
   const publicApi_Url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`;
 
+  // Make a get request to access/view NASA API data
   const response = axios.get(`${publicApi_Url}`, {
     withCredentials: false,
   });
@@ -18,6 +22,7 @@ export default function PublicApiMars() {
   const [title, setTitle] = useState();
   const [date, setDate] = useState();
 
+  // retrieve all relevant API data from NASA API
   async function fetchData() {
     setTitle((await response).data.photos[0].camera.full_name);
     setUrl((await response).data.photos[0].img_src);
@@ -30,6 +35,7 @@ export default function PublicApiMars() {
       <h1>{title}</h1>
       <img src={url} />
       <p>Date of capture: {date}</p>
+      <Button />
     </div>
   );
 }
